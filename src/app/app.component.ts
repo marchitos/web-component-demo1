85506ba3862c14082dec6167f5972ff1634dcf66
Marco Amadori <mamadori@vivocha.com>
2020-11-16 11:52:16 +0100

    init done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,13 @@ import {Component, ElementRef, ViewChild, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Infocert';
+  jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJhbGVzc2FuZHJvIiwiZXhwIjoxNjA2MTQ5MTU2fQ.9KnxtyD8LHhJRqyq5HJH01XQxYGpO_W6S_ShEjMmliY';
   ngOnInit() {
+  }
+  vivochaEvent(evt) {
+    if (evt && evt.detail) {
+      const ev = evt.detail;
+      console.log('VVC-WC', ev);
+    }
   }
 }
